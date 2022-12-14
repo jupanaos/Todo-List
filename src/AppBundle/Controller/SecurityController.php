a84990623fbd267c6d898e9ce2c2a76ada835c15
jupanaos <julie.pariona@gmail.com>
2022-12-14 18:00:06 +0100

    feat: #6 cs fixer installed

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 class SecurityController extends Controller
 {
     /**
