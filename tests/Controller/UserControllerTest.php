1958a63705a7132b66b48709b10878dc20cd8025
jupanaos <julie.pariona@gmail.com>
2022-12-28 00:21:20 +0100

    test: user list/create/edit OK

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -23,12 +23,10 @@ class UserControllerTest extends WebTestCase
      * Test users list access as user 
      * @return void
      */
-    public function testListAsUser(): void
+    public function testUserListAsUser(): void
     {
         $client = static::createClient();
-
         $userRepository = static::getContainer()->get(UserRepository::class);
-
         $testUser = $userRepository->findOneBy(['email' => 'user1@todo.fr']);
 
         $client
@@ -36,5 +34,82 @@ class UserControllerTest extends WebTestCase
             ->request('GET', '/users');
 
         $this->assertEquals(403, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Test users list access as admin
+     * @return void
+     */
+    public function testUserListAsAdmin(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testAdmin = $userRepository->findOneBy(['email' => 'admin@todo.fr']);
+
+        $client
+            ->loginUser($testAdmin)
+            ->request('GET', '/users');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Test user creation
+     * @return void
+     */
+    public function testUserCreate(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testAdmin = $userRepository->findOneBy(['email' => 'admin@todo.fr']);
+
+        $client
+            ->loginUser($testAdmin)
+            ->request('GET', '/users/create');
+        $client->submitForm(
+                'Créer l\'utilisateur',
+                [
+                    'user[username]' => 'username-test',
+                    'user[plainPassword][first]' => 'todo-test',
+                    'user[plainPassword][second]' => 'todo-test',
+                    'user[email]' => 'email@test.fr',
+                    'user[roles]' => 'ROLE_USER'
+                ]
+            );
+        $client->followRedirects();
+
+        $this->assertResponseRedirects('/users', 302);
+        $this->assertNotNull($userRepository->findOneBy(['email' => 'email@test.fr']));
+    }
+
+    public function testUserEdit(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $testAdmin = $userRepository->findOneBy(['email' => 'admin@todo.fr']);
+        $testUser = $userRepository->findOneBy(['email' => 'user2@todo.fr']);
+
+        $client->loginUser($testAdmin);
+        $client->request('GET', '/users/'.$testUser->getId().'/edit');
+
+        $client->submitForm(
+            'Editer',
+            [
+            'user[username]' => 'user2-edit',
+            'user[plainPassword][first]' => 'todo-test',
+            'user[plainPassword][second]' => 'todo-test',
+            'user[email]' => 'user2-edit@todo.fr',
+            'user[roles]' => 'ROLE_USER',
+            ]
+        );
+        $client->followRedirects();
+
+        $testUserEdited = $userRepository->find($testUser->getId());
+        $this->assertResponseRedirects('/users', 302);
+        $this->assertNotNull($userRepository->findOneBy(['email' => 'user2-edit@todo.fr']));
+        $this->assertNull($userRepository->findOneBy(['email' => 'user2@todo.fr']));
+        $this->assertSame('user2-edit', $testUserEdited->getUsername());
+        $this->assertSame('user2-edit@todo.fr', $testUserEdited->getEmail());
     }
 }
